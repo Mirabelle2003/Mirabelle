3c098fec2e29ec69c7e77285054fdbddae1597ff
Mirabelle2003 <miradahhede@gmail.com>
2022-08-30 13:00:54 +0200

    fichiers modifiés

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mon_projet/home/home.dart';
+import 'package:mon_projet/jeu.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: const Color(0XFF4584FF),
       ),
-      home: const MyHomePage(),
+      home: const JeuDesScreen(),
     );
   }
 }
